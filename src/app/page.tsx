2e0c257a6761ca97d6fc5375a76ade8b128bbb98
Jackson <jackson.borba@sisplansistemas.com.br>
2023-09-20 13:08:20 -0300

    Ajustado navegacao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import { BsFillHandbagFill   } from "react-icons/bs";
 import { BiSolidUser } from "react-icons/bi";
 import ProductCard from "./Product/ProductCard";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<div>
 			<section className="fixed top-0 flex h-10 w-full items-center justify-between bg-red-500 px-4">
-				<div className="flex w-80 space-x-10 px-5">
-					<button className="text-2xl">Inicio</button>
-					<button className="text-xs text-gray-300">Calças</button>
-					<button className="text-xs text-gray-300">Camisas</button>
+				<div className="flex items-center w-80 space-x-10 px-5">
+					<Link href="/">
+						<div className="text-2xl">Início</div>
+					</Link>
+					<Link href="/calcas">
+						<div className="text-xs text-gray-300">Calças</div>
+					</Link>
+					<Link href="/camisas">
+						<div className="text-xs text-gray-300">Camisas</div>
+					</Link>
 				</div>
 				<input type="text" placeholder="Pesquisar" className="w-96 rounded-full border border-gray-300 px-2 focus:border-gray-500 focus:outline-none" />
 				<div className="flex items-start justify-center">
